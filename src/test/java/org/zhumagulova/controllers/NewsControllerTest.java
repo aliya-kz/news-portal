44e26a07a9d44f2163c79c9d2b8ded4faae919b7
zhumagul100 <zhumagul100@gmail.com>
2022-06-24 17:42:34 +0600

    minor changes

--- src/test/java/org/zhumagulova/controllers/NewsControllerTest.java
+++ src/test/java/org/zhumagulova/controllers/NewsControllerTest.java
@@ -6,7 +6,6 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.MockMvcBuilder;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.zhumagulova.models.News;
 import org.zhumagulova.service.NewsService;
@@ -16,7 +15,7 @@ import java.util.List;
 
 import static org.hamcrest.Matchers.hasSize;
 import static org.hamcrest.Matchers.instanceOf;
-import static org.junit.jupiter.api.Assertions.*;
+
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
